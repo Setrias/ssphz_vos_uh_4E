4f1e8c1a87572608705be898572f90acd257b11f
setrias <tomas.buchtik@borsice.net>
2023-09-12 13:23:06 +0200

    initial commit, opakováni_4E

---
+++ opakovani_4E/opakovani_4E/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace opakovani_4E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCislo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPrecist = new System.Windows.Forms.Button();
+            this.textBoxVysledek = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSlovo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHeslo = new System.Windows.Forms.TextBox();
+            this.buttonHeslo = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxDen = new System.Windows.Forms.TextBox();
+            this.textBoxMesic = new System.Windows.Forms.TextBox();
+            this.textBoxRok = new System.Windows.Forms.TextBox();
+            this.textBoxNazevDne = new System.Windows.Forms.TextBox();
+            this.buttonVypsat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCislo
+            // 
+            this.textBoxCislo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCislo.Location = new System.Drawing.Point(81, 51);
+            this.textBoxCislo.Name = "textBoxCislo";
+            this.textBoxCislo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCislo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Číslo:";
+            // 
+            // buttonPrecist
+            // 
+            this.buttonPrecist.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPrecist.Location = new System.Drawing.Point(187, 51);
+            this.buttonPrecist.Name = "buttonPrecist";
+            this.buttonPrecist.Size = new System.Drawing.Size(75, 52);
+            this.buttonPrecist.TabIndex = 2;
+            this.buttonPrecist.Text = "Vypočítat";
+            this.buttonPrecist.UseVisualStyleBackColor = true;
+            this.buttonPrecist.Click += new System.EventHandler(this.buttonPrecist_Click);
+            // 
+            // textBoxVysledek
+            // 
+            this.textBoxVysledek.Enabled = false;
+            this.textBoxVysledek.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxVysledek.Location = new System.Drawing.Point(81, 80);
+            this.textBoxVysledek.Name = "textBoxVysledek";
+            this.textBoxVysledek.Size = new System.Drawing.Size(100, 23);
+            this.textBoxVysledek.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Opakování";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Výsledek:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Slovo:";
+            // 
+            // textBoxSlovo
+            // 
+            this.textBoxSlovo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSlovo.Location = new System.Drawing.Point(81, 134);
+            this.textBoxSlovo.Name = "textBoxSlovo";
+            this.textBoxSlovo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxSlovo.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Heslo:";
+            // 
+            // textBoxHeslo
+            // 
+            this.textBoxHeslo.Enabled = false;
+            this.textBoxHeslo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHeslo.Location = new System.Drawing.Point(81, 163);
+            this.textBoxHeslo.Name = "textBoxHeslo";
+            this.textBoxHeslo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHeslo.TabIndex = 9;
+            // 
+            // buttonHeslo
+            // 
+            this.buttonHeslo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonHeslo.Location = new System.Drawing.Point(187, 134);
+            this.buttonHeslo.Name = "buttonHeslo";
+            this.buttonHeslo.Size = new System.Drawing.Size(75, 52);
+            this.buttonHeslo.TabIndex = 10;
+            this.buttonHeslo.Text = "Generovat";
+            this.buttonHeslo.UseVisualStyleBackColor = true;
+            this.buttonHeslo.Click += new System.EventHandler(this.buttonHeslo_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Den:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(13, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Měsíc:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Rok:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Název dne:";
+            // 
+            // textBoxDen
+            // 
+            this.textBoxDen.Enabled = false;
+            this.textBoxDen.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDen.Location = new System.Drawing.Point(81, 211);
+            this.textBoxDen.Name = "textBoxDen";
+            this.textBoxDen.Size = new System.Drawing.Size(100, 23);
+            this.textBoxDen.TabIndex = 15;
+            // 
+            // textBoxMesic
+            // 
+            this.textBoxMesic.Enabled = false;
+            this.textBoxMesic.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMesic.Location = new System.Drawing.Point(81, 240);
+            this.textBoxMesic.Name = "textBoxMesic";
+            this.textBoxMesic.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMesic.TabIndex = 16;
+            // 
+            // textBoxRok
+            // 
+            this.textBoxRok.Enabled = false;
+            this.textBoxRok.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRok.Location = new System.Drawing.Point(81, 269);
+            this.textBoxRok.Name = "textBoxRok";
+            this.textBoxRok.Size = new System.Drawing.Size(100, 23);
+            this.textBoxRok.TabIndex = 17;
+            // 
+            // textBoxNazevDne
+            // 
+            this.textBoxNazevDne.Enabled = false;
+            this.textBoxNazevDne.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNazevDne.Location = new System.Drawing.Point(81, 298);
+            this.textBoxNazevDne.Name = "textBoxNazevDne";
+            this.textBoxNazevDne.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNazevDne.TabIndex = 18;
+            // 
+            // buttonVypsat
+            // 
+            this.buttonVypsat.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonVypsat.Location = new System.Drawing.Point(187, 211);
+            this.buttonVypsat.Name = "buttonVypsat";
+            this.buttonVypsat.Size = new System.Drawing.Size(75, 110);
+            this.buttonVypsat.TabIndex = 19;
+            this.buttonVypsat.Text = "Vypsat";
+            this.buttonVypsat.UseVisualStyleBackColor = true;
+            this.buttonVypsat.Click += new System.EventHandler(this.buttonVypsat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonVypsat);
+            this.Controls.Add(this.textBoxNazevDne);
+            this.Controls.Add(this.textBoxRok);
+            this.Controls.Add(this.textBoxMesic);
+            this.Controls.Add(this.textBoxDen);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonHeslo);
+            this.Controls.Add(this.textBoxHeslo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxSlovo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxVysledek);
+            this.Controls.Add(this.buttonPrecist);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCislo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCislo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPrecist;
+        private System.Windows.Forms.TextBox textBoxVysledek;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSlovo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHeslo;
+        private System.Windows.Forms.Button buttonHeslo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxDen;
+        private System.Windows.Forms.TextBox textBoxMesic;
+        private System.Windows.Forms.TextBox textBoxRok;
+        private System.Windows.Forms.TextBox textBoxNazevDne;
+        private System.Windows.Forms.Button buttonVypsat;
+    }
+}
+
---
+++ opakovani_4E/opakovani_4E/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opakovani_4E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double cislo;
+
+        private void buttonPrecist_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cislo = Convert.ToDouble(textBoxCislo.Text);
+
+                if (cislo > 0)
+                {
+                    textBoxVysledek.Text = Convert.ToString(100 / cislo);
+                }
+                else
+                {
+                    MessageBox.Show("Číslo není kladné.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nebylo zadáno číslo.");
+            }
+        }
+
+        private String slovo;
+
+        private void buttonHeslo_Click(object sender, EventArgs e)
+        {
+            slovo = textBoxSlovo.Text;
+            if (slovo.Length < 4)
+            {
+                MessageBox.Show("Zadej delší slovo.");
+                return;
+            }
+
+            textBoxHeslo.Text = slovo.Substring(2, 2) + slovo[0] + DateTime.Now.Year;
+        }
+
+        private void buttonVypsat_Click(object sender, EventArgs e)
+        {
+            textBoxDen.Text = Convert.ToString(DateTime.Now.Day);
+            textBoxMesic.Text = Convert.ToString(DateTime.Now.Month);
+            textBoxRok.Text = Convert.ToString(DateTime.Now.Year);
+
+            int cisloDne = Convert.ToInt32(DateTime.Now.DayOfWeek);
+            string nazevDne = "";
+
+            switch (cisloDne)
+            {
+                case 0:
+                    nazevDne = "Neděle";
+                    break;
+                case 1:
+                    nazevDne = "Pondělí";
+                    break;
+                case 2:
+                    nazevDne = "Úterý";
+                    break;
+                case 3:
+                    nazevDne = "Středa";
+                    break;
+                case 4:
+                    nazevDne = "Čtvrtek";
+                    break;
+                case 5:
+                    nazevDne = "Pátek";
+                    break;
+                case 6:
+                    nazevDne = "Sobota";
+                    break;
+            }
+
+            textBoxNazevDne.Text = nazevDne;
+        }
+    }
+}
