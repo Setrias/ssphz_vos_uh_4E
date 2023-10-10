f14987bec3f1c8b3ef5db0f1de04917f35c403ca
Setrias <tomas.buchtik@borsice.net>
2023-10-10 10:36:54 +0200

    ukládání čísel

--- ukladani_cisel/ukladani_cisel/Form1.cs
+++ ukladani_cisel/ukladani_cisel/Form1.cs
@@ -15,44 +15,109 @@ namespace ukladani_cisel
         public Form1()
         {
             InitializeComponent();
+            ActiveControl = textBoxZadavani;
+            AcceptButton = buttonZadej;
         }
-        
+
         // Program čte celá čísla, ukládá je do pole. Zadávání skončí po stisku klávesy 'x' nebo 'X'.
         //      a) Vypsat čísla se sudými indexy.
         //      b) Největší číslo z čísel s lichými indexy.
         //      c) Počet čísel větších než 100.
         //      d) Nejmenší kladné číslo.
 
-        private int[] poleCisel = {};
+        //private int[] poleCisel = { };
+        private List<int> poleCisel = new List<int>();
         private int pocetZadani;
-        
+
         private void buttonZadej_Click(object sender, EventArgs e)
         {
             var zadanaHodnota = textBoxZadavani.Text;
-            
-            if (zadanaHodnota != "X" || zadanaHodnota != "x")
+
+            if (zadanaHodnota == "X" || zadanaHodnota == "x")
+            {
+                buttonZadej.Enabled = false;
+                KonecZadavani();
+            }
+            else
             {
                 try
                 {
                     int zadaneCislo = Convert.ToInt32(zadanaHodnota);
-                    poleCisel[pocetZadani] = zadaneCislo;
+                    poleCisel.Add(zadaneCislo);
+                    textBoxZadavani.Text = "";
                     pocetZadani++;
                 }
                 catch
                 {
-                    MessageBox.Show("Povoleny jsou pouze čísla nebo 'x', 'X'.");
+                    MessageBox.Show("Povoleny jsou pouze čísla nebo písmeno X.");
                 }
-            }
-            else
-            {
-                buttonZadej.Enabled = false;
-                KonecZadavani();
             }
         }
 
         private void KonecZadavani()
         {
-            
+            sudeCisla();
+            nejvetsiLiche();
+            vetsiSto();
+            nejmensiKladne();
+        }
+
+        private void sudeCisla()
+        {
+            for (int i = 0; i < poleCisel.Count; i++)
+            {
+                int cislo = poleCisel[i];
+                if (cislo % 2 == 0)
+                {
+                    listBoxSude.Items.Add(cislo);
+                }
+            }
+        }
+
+        private void nejvetsiLiche()
+        {
+            List<int> licheCisla = new List<int>();
+
+            for (int i = 0; i < poleCisel.Count; i++)
+            {
+                int cislo = poleCisel[i];
+                if (cislo % 2 != 0)
+                {
+                    licheCisla.Add(cislo);
+                }
+            }
+
+            textBoxMaxLiche.Text = Convert.ToString(poleCisel.Max());
+        }
+
+        private void vetsiSto()
+        {
+            List<int> nadSto = new List<int>();
+
+            foreach (var cislo in poleCisel)
+            {
+                if (cislo > 100)
+                {
+                    nadSto.Add(cislo);
+                }
+            }
+
+            textBoxVetsiSto.Text = Convert.ToString(nadSto.Count);
+        }
+
+        private void nejmensiKladne()
+        {
+            List<int> kladne = new List<int>();
+
+            foreach (var cislo in poleCisel)
+            {
+                if (cislo > 0)
+                {
+                    kladne.Add(cislo);
+                }
+            }
+
+            textBoxMinKladne.Text = Convert.ToString(kladne.Min());
         }
     }
 }
