21cf3984ae8160eca188d420fbd161427f0193b1
Setrias <tomas.buchtik@borsice.net>
2023-11-14 20:43:16 +0100

    Kreslení kvadratické funkce

--- kvadraticka_funkce/kvadraticka_funkce/Form1.Designer.cs
+++ kvadraticka_funkce/kvadraticka_funkce/Form1.Designer.cs
@@ -31,6 +31,16 @@
         {
             this.panelKvadraticka = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonVykreslit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxMeritko = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // panelKvadraticka
@@ -53,19 +63,135 @@
             this.label1.Text = "Vykreslení kvadratické funkce";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(35, 132);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 23);
+            this.textBoxA.TabIndex = 2;
+            this.textBoxA.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "a:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b:";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(35, 161);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 23);
+            this.textBoxB.TabIndex = 5;
+            this.textBoxB.Text = "0";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(35, 190);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 23);
+            this.textBoxC.TabIndex = 6;
+            this.textBoxC.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c:";
+            // 
+            // buttonVykreslit
+            // 
+            this.buttonVykreslit.Location = new System.Drawing.Point(12, 219);
+            this.buttonVykreslit.Name = "buttonVykreslit";
+            this.buttonVykreslit.Size = new System.Drawing.Size(123, 38);
+            this.buttonVykreslit.TabIndex = 8;
+            this.buttonVykreslit.Text = "Vykreslit";
+            this.buttonVykreslit.UseVisualStyleBackColor = true;
+            this.buttonVykreslit.Click += new System.EventHandler(this.buttonVykreslit_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "ax^2 + bx + c = 0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Faktor měřítka:";
+            // 
+            // comboBoxMeritko
+            // 
+            this.comboBoxMeritko.FormattingEnabled = true;
+            this.comboBoxMeritko.Items.AddRange(new object[] { "1x (Bez změny měřítka)", "2x", "3x", "4x" });
+            this.comboBoxMeritko.Location = new System.Drawing.Point(260, 132);
+            this.comboBoxMeritko.Name = "comboBoxMeritko";
+            this.comboBoxMeritko.Size = new System.Drawing.Size(155, 24);
+            this.comboBoxMeritko.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1264, 621);
+            this.Controls.Add(this.comboBoxMeritko);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonVykreslit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxA);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panelKvadraticka);
             this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Location = new System.Drawing.Point(15, 15);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Vykreslení kvadratické funkce";
+            this.Text = "Kvadratická funkce";
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.ComboBox comboBoxMeritko;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonVykreslit;
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
 
         private System.Windows.Forms.Label label1;
 
--- kvadraticka_funkce/kvadraticka_funkce/Form1.cs
+++ kvadraticka_funkce/kvadraticka_funkce/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kvadraticka_funkce
@@ -15,15 +9,134 @@ namespace kvadraticka_funkce
         public Form1()
         {
             InitializeComponent();
+            comboBoxMeritko.SelectedIndex = 0;
         }
 
         private Graphics panelFunkce;
-        KvadratickaFunkce funkce = new KvadratickaFunkce();
+        private int width = 600;
+        private int height = 600;
 
         private void panelKvadraticka_Paint(object sender, PaintEventArgs e)
         {
             panelFunkce = panelKvadraticka.CreateGraphics();
-            funkce.OsaGrafu(600, 600, panelFunkce);
+            OsaGrafu(width, height, panelFunkce);
+        }
+
+        // univerzální nakreslení osy x a y
+        public void OsaGrafu(float width, float height, Graphics panel)
+        {
+            Pen blackAxisPen = new Pen(Color.Black, 2);
+            Pen brownAxisPen = new Pen(Color.Brown, 2);
+            Pen grayAxisPen = new Pen(Color.LightGray, 2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                // síť pozadí
+                panel.DrawLine(grayAxisPen, 0, 0 + i * (height / 10), width, 0 + i * (height / 10));
+                // čárky na vertikální ose
+                panel.DrawLine(blackAxisPen, width / 2 - 10, 0 + i * (height / 10), width / 2 + 10, 0 + i * (height / 10));
+                panel.DrawLine(brownAxisPen, width / 2 - 5, (height / 20) + i * (height / 10), width / 2 + 5, (height / 20) + i * (height / 10));
+
+                // síť pozadí
+                panel.DrawLine(grayAxisPen, 0 + i * (width / 10), 0, 0 + i * (width / 10), height);
+                // čárky na horizontální ose
+                panel.DrawLine(blackAxisPen, 0 + i * (width / 10), height / 2 - 10, 0 + i * (width / 10), height / 2 + 10);
+                panel.DrawLine(brownAxisPen, (width / 20) + i * (width / 10), height / 2 - 5, (width / 20) + i * (width / 10), height / 2 + 5);
+            }
+
+            // vertikální
+            panel.DrawLine(blackAxisPen, width / 2, 0, width / 2, height);
+            // horizontální
+            panel.DrawLine(blackAxisPen, 0, height / 2, width, height / 2);
+
+            // vertikální poslední čára
+            panel.DrawLine(Pens.Black, width / 2 - 10, height - 3, width / 2 + 10, height - 3);
+            // horizontální poslední čára
+            panel.DrawLine(Pens.Black, width - 3, height / 2 - 10, width - 3, height / 2 + 10);
+        }
+
+        private float clenA, clenB, clenC;
+        private int meritko;
+
+        private void buttonVykreslit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clenA = Convert.ToSingle(textBoxA.Text);
+                clenB = Convert.ToSingle(textBoxB.Text);
+                clenC = Convert.ToSingle(textBoxC.Text);
+                meritko = comboBoxMeritko.SelectedIndex;
+
+                switch (meritko)
+                {
+                    case 0:
+                        meritko = 10;
+                        break;
+                    case 1:
+                        meritko = 20;
+                        break;
+                    case 2:
+                        meritko = 30;
+                        break;
+                    case 3:
+                        meritko = 40;
+                        break;
+                }
+
+                if (clenA == 0)
+                {
+                    MessageBox.Show("Člen 'a' nesmí být 0!");
+                    return;
+                }
+
+                panelKvadraticka.Refresh();
+                DrawQuadraticFunction(clenA, clenB, clenC, meritko, panelFunkce);
+            }
+            catch
+            {
+                MessageBox.Show("Zadaná hodnota musí být číslo!");
+            }
+        }
+
+        public void DrawQuadraticFunction(float numA, float numB, float numC, int meritko, Graphics panel)
+        {
+            // Definice parametrů kvadratické funkce
+            float a = numA; // koeficient a
+            float b = numB; // koeficient b
+            float c = numC; // koeficient c
+
+            // Definice rozsahu x-ové osy
+            float startX = -20;
+            float endX = 20;
+
+            // Definice šířky a výšky kreslící plochy
+
+            float prevX = 0, prevY = 0;
+            bool prvni = false;
+
+            Pen pero = new Pen(Color.Red, 2);
+
+            // Kreslení kvadratické funkce
+            for (float x = startX; x < endX; x += Convert.ToSingle(0.1))
+            {
+                float y = a * x * x + b * x + c;
+
+                // Převod souřadnic do pixelů
+                float pixelX = width / 2 + x * meritko;
+                float pixelY = height / 2 - y * meritko;
+
+                if (prvni)
+                {
+                    panel.DrawLine(pero, prevX, prevY, pixelX, pixelY);
+                }
+                else
+                {
+                    prvni = true;
+                }
+
+                prevX = pixelX;
+                prevY = pixelY;
+            }
         }
     }
 }
