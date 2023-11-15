0ace572fee8d326e2bcf7d6671e7f3ebcebcf60b
setrias <tomas.buchtik@borsice.net>
2023-11-15 14:19:33 +0100

    kvadratická funkce - škola

---
+++ kvadraticka_funkce_skola/kvadraticka_funkce_skola/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace kvadraticka_funkce_skola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelKvadraticka = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonVykreslit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelKvadraticka
+            // 
+            this.panelKvadraticka.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelKvadraticka.Location = new System.Drawing.Point(388, 15);
+            this.panelKvadraticka.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelKvadraticka.Name = "panelKvadraticka";
+            this.panelKvadraticka.Size = new System.Drawing.Size(400, 600);
+            this.panelKvadraticka.TabIndex = 0;
+            this.panelKvadraticka.Paint += new System.Windows.Forms.PaintEventHandler(this.panelKvadraticka_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kvadratická funkce";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "a:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(35, 70);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 23);
+            this.textBoxA.TabIndex = 3;
+            this.textBoxA.Text = "1";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(35, 99);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 23);
+            this.textBoxB.TabIndex = 4;
+            this.textBoxB.Text = "0";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(35, 128);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 23);
+            this.textBoxC.TabIndex = 5;
+            this.textBoxC.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c:";
+            // 
+            // buttonVykreslit
+            // 
+            this.buttonVykreslit.Location = new System.Drawing.Point(12, 157);
+            this.buttonVykreslit.Name = "buttonVykreslit";
+            this.buttonVykreslit.Size = new System.Drawing.Size(123, 34);
+            this.buttonVykreslit.TabIndex = 8;
+            this.buttonVykreslit.Text = "Vykreslit funkci";
+            this.buttonVykreslit.UseVisualStyleBackColor = true;
+            this.buttonVykreslit.Click += new System.EventHandler(this.buttonVykreslit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 770);
+            this.Controls.Add(this.buttonVykreslit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelKvadraticka);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonVykreslit;
+
+        private System.Windows.Forms.Panel panelKvadraticka;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ kvadraticka_funkce_skola/kvadraticka_funkce_skola/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kvadraticka_funkce_skola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            width = panelKvadraticka.Size.Width;
+            height = panelKvadraticka.Size.Height;
+        }
+
+        private int width, height;
+        private Graphics panel;
+        private float clenA, clenB, clenC;
+
+        private void panelKvadraticka_Paint(object sender, PaintEventArgs e)
+        {
+            panel = panelKvadraticka.CreateGraphics();
+
+            panel.DrawLine(Pens.Black, width / 2, 0, width / 2, height);
+            panel.DrawLine(Pens.Black, 0, height / 2, width, height / 2);
+        }
+
+        private bool prvni = false;
+        private float prevX, prevY;
+
+        private Pen pero = new Pen(Color.Crimson, 2);
+
+        private void buttonVykreslit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clenA = Convert.ToSingle(textBoxA.Text);
+                clenB = Convert.ToSingle(textBoxB.Text);
+                clenC = Convert.ToSingle(textBoxC.Text);
+
+                if (clenA == 0)
+                {
+                    MessageBox.Show("Člen 'a' nesmí být 0.");
+                    return;
+                }
+
+                panelKvadraticka.Refresh();
+                float prevX = 0, prevY = 0;
+                bool prvni = false;
+
+                for (float x = -20; x <= 20; x += Convert.ToSingle(0.1))
+                {
+                    float y = clenA * x * x + clenB * x + clenC;
+
+                    float pixelX = width / 2 + x * 10;
+                    float pixelY = height / 2 - y * 10;
+
+                    if (prvni)
+                    {
+                        panel.DrawLine(pero, prevX, prevY, pixelX, pixelY);
+                    }
+                    else
+                    {
+                        prvni = true;
+                    }
+
+                    prevX = pixelX;
+                    prevY = pixelY;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Zadaná hodnota musí být číslo.");
+            }
+        }
+    }
+}
