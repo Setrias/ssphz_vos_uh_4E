eca0630630a0a6d1e5595be61da556ccef140bed
setrias <tomas.buchtik@borsice.net>
2023-11-28 13:30:18 +0100

    Sinus úpravy + součet sinu

--- sinus_funkce/sinus_funkce/Form1.Designer.cs
+++ sinus_funkce/sinus_funkce/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panelSinus
@@ -118,7 +119,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Blue;
             this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(12, 27);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(109, 19);
@@ -128,7 +131,9 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Red;
             this.label5.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(311, 27);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(115, 19);
@@ -193,11 +198,24 @@
             this.label8.TabIndex = 9;
             this.label8.Text = "Amplituda:";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label9.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(12, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 19);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Součet sinusovek";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 605);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.buttonVykreslitDruhou);
             this.Controls.Add(this.textBoxFiDruhe);
@@ -222,6 +240,8 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label9;
 
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button buttonVykreslitDruhou;
