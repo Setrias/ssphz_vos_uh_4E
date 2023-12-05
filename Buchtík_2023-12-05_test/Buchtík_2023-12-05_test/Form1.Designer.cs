1063ea30d0020471803778f3c1ca2e3eb98a9504
setrias <tomas.buchtik@borsice.net>
2023-12-05 13:52:21 +0100

    Písemka 2023-12-05, funkce cosinus

---
+++ Buchtík_2023-12-05_test/Buchtík_2023-12-05_test/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Buchtík_2023_12_05_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGraf = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAmplituda = new System.Windows.Forms.TextBox();
+            this.textBoxPerioda = new System.Windows.Forms.TextBox();
+            this.buttonVykreslit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelGraf
+            // 
+            this.panelGraf.Location = new System.Drawing.Point(186, 34);
+            this.panelGraf.Name = "panelGraf";
+            this.panelGraf.Size = new System.Drawing.Size(602, 379);
+            this.panelGraf.TabIndex = 0;
+            this.panelGraf.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGraf_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Amplituda:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Perioda:";
+            // 
+            // textBoxAmplituda
+            // 
+            this.textBoxAmplituda.Location = new System.Drawing.Point(80, 34);
+            this.textBoxAmplituda.Name = "textBoxAmplituda";
+            this.textBoxAmplituda.Size = new System.Drawing.Size(100, 23);
+            this.textBoxAmplituda.TabIndex = 3;
+            // 
+            // textBoxPerioda
+            // 
+            this.textBoxPerioda.Location = new System.Drawing.Point(80, 63);
+            this.textBoxPerioda.Name = "textBoxPerioda";
+            this.textBoxPerioda.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPerioda.TabIndex = 4;
+            // 
+            // buttonVykreslit
+            // 
+            this.buttonVykreslit.Location = new System.Drawing.Point(12, 92);
+            this.buttonVykreslit.Name = "buttonVykreslit";
+            this.buttonVykreslit.Size = new System.Drawing.Size(168, 48);
+            this.buttonVykreslit.TabIndex = 5;
+            this.buttonVykreslit.Text = "Vykreslit";
+            this.buttonVykreslit.UseVisualStyleBackColor = true;
+            this.buttonVykreslit.Click += new System.EventHandler(this.buttonVykreslit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.buttonVykreslit);
+            this.Controls.Add(this.textBoxPerioda);
+            this.Controls.Add(this.textBoxAmplituda);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelGraf);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Cos funkce";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Panel panelGraf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAmplituda;
+        private System.Windows.Forms.TextBox textBoxPerioda;
+        private System.Windows.Forms.Button buttonVykreslit;
+
+        #endregion
+    }
+}
---
+++ Buchtík_2023-12-05_test/Buchtík_2023-12-05_test/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buchtík_2023_12_05_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics panelFunkce;
+        private int width, height;
+
+        private int amplituda;
+        private float perioda;
+        private Pen peroBlue = new Pen(Color.Blue, 2);
+
+        private void buttonVykreslit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                amplituda = Convert.ToInt32(textBoxAmplituda.Text);
+                perioda = Convert.ToSingle(textBoxPerioda.Text);
+
+                // kontrola proměnných
+                if (perioda >= 0.5 && perioda <= 1.5 && amplituda > 0)
+                {
+                    Vykreslit();
+                }
+                else
+                {
+                    MessageBox.Show("Perioda musí být z rozmezí 0.5 až 1.5 a amplituda musí být větší než 0.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Zadej číslo.");
+            }
+        }
+
+        private void Vykreslit()
+        {
+            // refresh panelu
+            panelGraf.Refresh();
+
+            // první bod
+            float prevY = Convert.ToSingle(amplituda * Math.Cos(2 * Math.PI * 0));
+
+            float krok = perioda / width;
+
+            for (int i = 1; i < width; i++)
+            {
+                // následující bod
+                float y = Convert.ToSingle(amplituda * Math.Cos(2 * Math.PI * i * krok));
+
+                // kreslní čáry
+                panelFunkce.DrawLine(peroBlue, i, prevY + height / 2, i + krok, y + height / 2);
+
+                prevY = y;
+            }
+        }
+
+        private void panelGraf_Paint(object sender, PaintEventArgs e)
+        {
+            panelFunkce = panelGraf.CreateGraphics();
+            
+            // šířka a výška panelu
+            width = panelGraf.Width;
+            height = panelGraf.Height;
+
+            // horizonální osa
+            panelFunkce.DrawLine(Pens.Black, 0, height / 2, width, height / 2);
+
+            // vertikální osa
+            panelFunkce.DrawLine(Pens.Black, 0, 0, 0, height);
+        }
+    }
+}
