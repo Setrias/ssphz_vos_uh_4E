34f109b7ebe00556cd3e1cdbeab49ef7b28edb12
setrias <tomas.buchtik@borsice.net>
2023-12-12 14:28:03 +0100

    Základ programu na harmonickou analýzu

---
+++ harmonicka_analyza/harmonicka_analyza/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace harmonicka_analyza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonZadat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownHodnoty = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxVysledek = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHodnoty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonZadat
+            // 
+            this.buttonZadat.Location = new System.Drawing.Point(12, 74);
+            this.buttonZadat.Name = "buttonZadat";
+            this.buttonZadat.Size = new System.Drawing.Size(301, 38);
+            this.buttonZadat.TabIndex = 0;
+            this.buttonZadat.Text = "Zadat";
+            this.buttonZadat.UseVisualStyleBackColor = true;
+            this.buttonZadat.Click += new System.EventHandler(this.buttonZadat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a:";
+            // 
+            // numericUpDownHodnoty
+            // 
+            this.numericUpDownHodnoty.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDownHodnoty.Location = new System.Drawing.Point(177, 41);
+            this.numericUpDownHodnoty.Name = "numericUpDownHodnoty";
+            this.numericUpDownHodnoty.Size = new System.Drawing.Size(136, 27);
+            this.numericUpDownHodnoty.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Harmonická analýza";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zadává se 10 hodnot";
+            // 
+            // textBoxVysledek
+            // 
+            this.textBoxVysledek.Location = new System.Drawing.Point(216, 118);
+            this.textBoxVysledek.Name = "textBoxVysledek";
+            this.textBoxVysledek.ReadOnly = true;
+            this.textBoxVysledek.Size = new System.Drawing.Size(97, 23);
+            this.textBoxVysledek.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Výsledek:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 157);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxVysledek);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownHodnoty);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonZadat);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Harmonická analýza";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHodnoty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxVysledek;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.NumericUpDown numericUpDownHodnoty;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button buttonZadat;
+
+        #endregion
+    }
+}
