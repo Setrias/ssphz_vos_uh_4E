088de318ff35ed0b59c3de615d2f493fdf74f6b8
setrias <tomas.buchtik@borsice.net>
2024-01-16 13:53:30 +0100

    Funkce Saxana

---
+++ funkceSaxany/funkceSaxany/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace funkceSaxany
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.buttonVypocitat = new System.Windows.Forms.Button();
+            this.textBoxVysledek = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Location = new System.Drawing.Point(12, 13);
+            this.numericUpDownX.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(148, 23);
+            this.numericUpDownX.TabIndex = 0;
+            // 
+            // buttonVypocitat
+            // 
+            this.buttonVypocitat.Location = new System.Drawing.Point(12, 43);
+            this.buttonVypocitat.Name = "buttonVypocitat";
+            this.buttonVypocitat.Size = new System.Drawing.Size(148, 27);
+            this.buttonVypocitat.TabIndex = 1;
+            this.buttonVypocitat.Text = "Vypočítat";
+            this.buttonVypocitat.UseVisualStyleBackColor = true;
+            this.buttonVypocitat.Click += new System.EventHandler(this.buttonVypocitat_Click);
+            // 
+            // textBoxVysledek
+            // 
+            this.textBoxVysledek.Location = new System.Drawing.Point(12, 76);
+            this.textBoxVysledek.Name = "textBoxVysledek";
+            this.textBoxVysledek.Size = new System.Drawing.Size(148, 23);
+            this.textBoxVysledek.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 124);
+            this.Controls.Add(this.textBoxVysledek);
+            this.Controls.Add(this.buttonVypocitat);
+            this.Controls.Add(this.numericUpDownX);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Funkce Saxana";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button buttonVypocitat;
+        private System.Windows.Forms.TextBox textBoxVysledek;
+
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+
+        #endregion
+    }
+}
---
+++ funkceSaxany/funkceSaxany/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace funkceSaxany
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int cislo;
+        private double sumaHodnot;
+
+        private void buttonVypocitat_Click(object sender, EventArgs e)
+        {
+            sumaHodnot = 0;
+            cislo = (int)numericUpDownX.Value;
+
+            for (int i = 1; i < 20; i++)
+            {
+                sumaHodnot += (Math.Pow(-1, i) * Math.Pow(cislo, 2 * i + 1)) / i;
+            }
+
+            textBoxVysledek.Text = Convert.ToString(sumaHodnot);
+        }
+    }
+}
