2aa4b4b19bce633732db1f600c176804f0c6e46f
setrias <tomas.buchtik@borsice.net>
2024-02-01 07:55:55 +0100

    Slunecni soustava

---
+++ slunecni_soustava/slunecni_soustava/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slunecni_soustava
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string nazevPlanety;
+        private double obeznaDoba;
+
+        private void buttonZadat_Click(object sender, EventArgs e)
+        {
+            nazevPlanety = textBoxNazevPlanety.Text;
+            try
+            {
+                obeznaDoba = Convert.ToDouble(textBoxObeznaDoba.Text);
+
+                if (obeznaDoba <= 0)
+                {
+                    MessageBox.Show("Oběžná doba musí být kladné číslo.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Oběžná doba musí být kladné reálné číslo");
+                return;
+            }
+        }
+    }
+}
---
+++ slunecni_soustava/slunecni_soustava/Planety.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace slunecni_soustava
+{
+    public class Planety
+    {
+        private double obeznaDoba, r_AU, r_km, rychlostTelesa;
+
+        public Planety(double iObeznaDoba)
+        {
+            obeznaDoba = iObeznaDoba;
+        }
+
+        public double VypisObezneDoby()
+        {
+            return obeznaDoba;
+        }
+
+        public double VypisVzdalenostAU()
+        {
+            r_AU = Math.Pow(Math.Pow(obeznaDoba, 2), 1 / 3.0); // třetí odmocnina je x na 1/3
+            return r_AU;
+        }
+
+        public double VypisVzdalenostKm()
+        {
+            r_km = Math.Pow(Math.Pow(obeznaDoba, 2), 1 / 3.0) * 149597871;
+            return r_km;
+        }
+
+        public double PrumernaRychlostTelesa()
+        {
+            rychlostTelesa = (2 * Math.PI * r_km) / obeznaDoba;
+            return rychlostTelesa;
+        }
+    }
+}
