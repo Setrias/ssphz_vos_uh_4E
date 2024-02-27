06d7033f4a6518d52de8017134a3ad976d1fe1f5
setrias <tomas.buchtik@borsice.net>
2024-02-27 14:33:25 +0100

    Harmonická analýza graf > WIP

--- harmonicka_analyza/harmonicka_analyza/Form1.Designer.cs
+++ harmonicka_analyza/harmonicka_analyza/Form1.Designer.cs
@@ -49,15 +49,17 @@
             this.label9 = new System.Windows.Forms.Label();
             this.textBoxB4 = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.panelHarmonicka = new System.Windows.Forms.Panel();
+            this.listBoxHodnoty = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // buttonVypocitat
             // 
-            this.buttonVypocitat.Location = new System.Drawing.Point(16, 54);
+            this.buttonVypocitat.Location = new System.Drawing.Point(12, 46);
             this.buttonVypocitat.Name = "buttonVypocitat";
-            this.buttonVypocitat.Size = new System.Drawing.Size(302, 38);
+            this.buttonVypocitat.Size = new System.Drawing.Size(159, 38);
             this.buttonVypocitat.TabIndex = 1;
-            this.buttonVypocitat.Text = "Zadat";
+            this.buttonVypocitat.Text = "Vypočítat a nakreslit";
             this.buttonVypocitat.UseVisualStyleBackColor = true;
             this.buttonVypocitat.Click += new System.EventHandler(this.buttonVypocitat_Click);
             // 
@@ -74,7 +76,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 101);
+            this.label1.Location = new System.Drawing.Point(13, 102);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 16);
             this.label1.TabIndex = 2;
@@ -82,24 +84,24 @@
             // 
             // textBoxA0
             // 
-            this.textBoxA0.Location = new System.Drawing.Point(65, 98);
+            this.textBoxA0.Location = new System.Drawing.Point(61, 99);
             this.textBoxA0.Name = "textBoxA0";
             this.textBoxA0.ReadOnly = true;
-            this.textBoxA0.Size = new System.Drawing.Size(97, 23);
+            this.textBoxA0.Size = new System.Drawing.Size(110, 23);
             this.textBoxA0.TabIndex = 3;
             // 
             // textBoxA1
             // 
-            this.textBoxA1.Location = new System.Drawing.Point(65, 127);
+            this.textBoxA1.Location = new System.Drawing.Point(61, 128);
             this.textBoxA1.Name = "textBoxA1";
             this.textBoxA1.ReadOnly = true;
-            this.textBoxA1.Size = new System.Drawing.Size(97, 23);
+            this.textBoxA1.Size = new System.Drawing.Size(110, 23);
             this.textBoxA1.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 130);
+            this.label3.Location = new System.Drawing.Point(13, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(21, 16);
             this.label3.TabIndex = 4;
@@ -107,16 +109,16 @@
             // 
             // textBoxA2
             // 
-            this.textBoxA2.Location = new System.Drawing.Point(65, 156);
+            this.textBoxA2.Location = new System.Drawing.Point(61, 157);
             this.textBoxA2.Name = "textBoxA2";
             this.textBoxA2.ReadOnly = true;
-            this.textBoxA2.Size = new System.Drawing.Size(97, 23);
+            this.textBoxA2.Size = new System.Drawing.Size(110, 23);
             this.textBoxA2.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 159);
+            this.label4.Location = new System.Drawing.Point(13, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 16);
             this.label4.TabIndex = 6;
@@ -124,16 +126,16 @@
             // 
             // textBoxA3
             // 
-            this.textBoxA3.Location = new System.Drawing.Point(65, 185);
+            this.textBoxA3.Location = new System.Drawing.Point(61, 186);
             this.textBoxA3.Name = "textBoxA3";
             this.textBoxA3.ReadOnly = true;
-            this.textBoxA3.Size = new System.Drawing.Size(97, 23);
+            this.textBoxA3.Size = new System.Drawing.Size(110, 23);
             this.textBoxA3.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 188);
+            this.label5.Location = new System.Drawing.Point(13, 189);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(23, 16);
             this.label5.TabIndex = 8;
@@ -141,16 +143,16 @@
             // 
             // textBoxA4
             // 
-            this.textBoxA4.Location = new System.Drawing.Point(65, 214);
+            this.textBoxA4.Location = new System.Drawing.Point(61, 215);
             this.textBoxA4.Name = "textBoxA4";
             this.textBoxA4.ReadOnly = true;
-            this.textBoxA4.Size = new System.Drawing.Size(97, 23);
+            this.textBoxA4.Size = new System.Drawing.Size(110, 23);
             this.textBoxA4.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(17, 217);
+            this.label6.Location = new System.Drawing.Point(13, 218);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(24, 16);
             this.label6.TabIndex = 10;
@@ -158,16 +160,16 @@
             // 
             // textBoxB1
             // 
-            this.textBoxB1.Location = new System.Drawing.Point(221, 127);
+            this.textBoxB1.Location = new System.Drawing.Point(61, 260);
             this.textBoxB1.Name = "textBoxB1";
             this.textBoxB1.ReadOnly = true;
-            this.textBoxB1.Size = new System.Drawing.Size(97, 23);
+            this.textBoxB1.Size = new System.Drawing.Size(110, 23);
             this.textBoxB1.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(173, 130);
+            this.label7.Location = new System.Drawing.Point(13, 263);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(21, 16);
             this.label7.TabIndex = 12;
@@ -175,16 +177,16 @@
             // 
             // textBoxB2
             // 
-            this.textBoxB2.Location = new System.Drawing.Point(221, 156);
+            this.textBoxB2.Location = new System.Drawing.Point(61, 289);
             this.textBoxB2.Name = "textBoxB2";
             this.textBoxB2.ReadOnly = true;
-            this.textBoxB2.Size = new System.Drawing.Size(97, 23);
+            this.textBoxB2.Size = new System.Drawing.Size(110, 23);
             this.textBoxB2.TabIndex = 15;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(173, 159);
+            this.label8.Location = new System.Drawing.Point(13, 292);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(23, 16);
             this.label8.TabIndex = 14;
@@ -192,16 +194,16 @@
             // 
             // textBoxB3
             // 
-            this.textBoxB3.Location = new System.Drawing.Point(221, 185);
+            this.textBoxB3.Location = new System.Drawing.Point(61, 318);
             this.textBoxB3.Name = "textBoxB3";
             this.textBoxB3.ReadOnly = true;
-            this.textBoxB3.Size = new System.Drawing.Size(97, 23);
+            this.textBoxB3.Size = new System.Drawing.Size(110, 23);
             this.textBoxB3.TabIndex = 17;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(173, 188);
+            this.label9.Location = new System.Drawing.Point(13, 321);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(23, 16);
             this.label9.TabIndex = 16;
@@ -209,26 +211,46 @@
             // 
             // textBoxB4
             // 
-            this.textBoxB4.Location = new System.Drawing.Point(221, 214);
+            this.textBoxB4.Location = new System.Drawing.Point(61, 347);
             this.textBoxB4.Name = "textBoxB4";
             this.textBoxB4.ReadOnly = true;
-            this.textBoxB4.Size = new System.Drawing.Size(97, 23);
+            this.textBoxB4.Size = new System.Drawing.Size(110, 23);
             this.textBoxB4.TabIndex = 19;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(173, 217);
+            this.label10.Location = new System.Drawing.Point(13, 350);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(24, 16);
             this.label10.TabIndex = 18;
             this.label10.Text = "b4:";
             // 
+            // panelHarmonicka
+            // 
+            this.panelHarmonicka.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelHarmonicka.Location = new System.Drawing.Point(189, 12);
+            this.panelHarmonicka.Name = "panelHarmonicka";
+            this.panelHarmonicka.Size = new System.Drawing.Size(573, 357);
+            this.panelHarmonicka.TabIndex = 20;
+            this.panelHarmonicka.Paint += new System.Windows.Forms.PaintEventHandler(this.panelHarmonicka_Paint);
+            // 
+            // listBoxHodnoty
+            // 
+            this.listBoxHodnoty.FormattingEnabled = true;
+            this.listBoxHodnoty.ItemHeight = 16;
+            this.listBoxHodnoty.Location = new System.Drawing.Point(779, 12);
+            this.listBoxHodnoty.Name = "listBoxHodnoty";
+            this.listBoxHodnoty.Size = new System.Drawing.Size(171, 356);
+            this.listBoxHodnoty.TabIndex = 21;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 254);
+            this.ClientSize = new System.Drawing.Size(962, 382);
+            this.Controls.Add(this.listBoxHodnoty);
+            this.Controls.Add(this.panelHarmonicka);
             this.Controls.Add(this.textBoxB4);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.textBoxB3);
@@ -256,6 +278,10 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ListBox listBoxHodnoty;
+
+        private System.Windows.Forms.Panel panelHarmonicka;
 
         private System.Windows.Forms.TextBox textBoxA2;
         private System.Windows.Forms.Label label4;
--- harmonicka_analyza/harmonicka_analyza/Form1.cs
+++ harmonicka_analyza/harmonicka_analyza/Form1.cs
@@ -17,26 +17,59 @@ namespace harmonicka_analyza
             InitializeComponent();
         }
 
+        // vypocty
         private double[] hodnoty = { 13, 25, 43, 56, 57, 28, 7, -14, -28, -29, -28, 3 };
         private double c = 12;
         private double sumaHodnotY, deltaAlfa;
+        private double koeficientA0;
+        private double frekvence = 50;
+
+        struct Koeficienty
+        {
+            public double a;
+            public double b;
+        }
+
+        private Koeficienty[] _koeficienty = new Koeficienty[4];
+
+        // graf
+        private Graphics grafHarmonicka;
+        private int panelHeight, panelWidth;
+
+        private void panelHarmonicka_Paint(object sender, PaintEventArgs e)
+        {
+            grafHarmonicka = panelHarmonicka.CreateGraphics();
+
+            panelHeight = panelHarmonicka.Height;
+            panelWidth = panelHarmonicka.Width;
+
+            grafHarmonicka.DrawLine(Pens.Black, 0, panelHeight / 2, panelWidth, panelHeight / 2);
+        }
 
         private void buttonVypocitat_Click(object sender, EventArgs e)
         {
+            panelHarmonicka.Refresh();
+
             sumaHodnotY = SumaHodnotY();
             deltaAlfa = 2 * Math.PI / c;
 
-            textBoxA0.Text = Convert.ToString(1.0 / c * sumaHodnotY);
+            koeficientA0 = 1.0 / c * sumaHodnotY;
+            textBoxA0.Text = Convert.ToString(koeficientA0);
 
-            textBoxA1.Text = VypocetAn(1).ToString();
-            textBoxA2.Text = VypocetAn(2).ToString();
-            textBoxA3.Text = VypocetAn(3).ToString();
-            textBoxA4.Text = VypocetAn(4).ToString();
+            VypocetAn();
+            VypocetBn();
 
-            textBoxB1.Text = VypocetBn(1).ToString();
-            textBoxB2.Text = VypocetBn(2).ToString();
-            textBoxB3.Text = VypocetBn(3).ToString();
-            textBoxB4.Text = VypocetBn(4).ToString();
+            textBoxA1.Text = _koeficienty[0].a.ToString();
+            textBoxA2.Text = _koeficienty[1].a.ToString();
+            textBoxA3.Text = _koeficienty[2].a.ToString();
+            textBoxA4.Text = _koeficienty[3].a.ToString();
+
+            textBoxB1.Text = _koeficienty[0].b.ToString();
+            textBoxB2.Text = _koeficienty[1].b.ToString();
+            textBoxB3.Text = _koeficienty[2].b.ToString();
+            textBoxB4.Text = _koeficienty[3].b.ToString();
+
+            KresleniGrafHarmonicka();
         }
 
         private double SumaHodnotY()
@@ -51,28 +84,60 @@ namespace harmonicka_analyza
             return sumaHodnotY;
         }
 
-        private double VypocetAn(int numHarmonicka)
+        private void VypocetAn()
         {
-            double suma = 0;
-
-            for (int i = 0; i < c; i++)
+            for (int i = 0; i < 4; i++)
             {
-                suma += hodnoty[i] * Math.Cos(numHarmonicka * ((i + 1) * deltaAlfa));
-            }
+                double suma = 0;
 
-            return 2.0 / c * suma;
+                for (int j = 0; j < c; j++)
+                {
+                    suma += hodnoty[j] * Math.Cos((i + 1) * ((j) * deltaAlfa));
+                }
+
+                _koeficienty[i].a = 2.0 / c * suma;
+            }
         }
 
-        private double VypocetBn(int numHarmonicka)
+        private void VypocetBn()
         {
-            double suma = 0;
-
-            for (int i = 0; i < c; i++)
+            for (int i = 0; i < 4; i++)
             {
-                suma += hodnoty[i] * Math.Sin(numHarmonicka * ((i + 1) * deltaAlfa));
-            }
+                double suma = 0;
 
-            return 2.0 / c * suma;
+                for (int j = 0; j < c; j++)
+                {
+                    suma += hodnoty[j] * Math.Sin((i + 1) * ((j) * deltaAlfa));
+                }
+
+                _koeficienty[i].b = 2.0 / c * suma;
+            }
+        }
+
+        private void KresleniGrafHarmonicka()
+        {
+            float yPrev = 0, yNext = 0;
+            int pocetAmplitud = 72;
+            int krok = panelWidth / pocetAmplitud;
+
+            double An = 0, Fin = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                An = Math.Sqrt(Math.Pow(_koeficienty[i].a, 2) + Math.Pow(_koeficienty[i].b, 2));
+                Fin = Math.Atan(_koeficienty[i].b / _koeficienty[i].a);
+
+                for (int j = 0; j < panelWidth; j += krok)
+                {
+                    yPrev += Convert.ToSingle(An * Math.Cos(i * (2 * Math.PI * frekvence) * j - Fin));
+                    yNext += Convert.ToSingle(An * Math.Cos(i * (2 * Math.PI * frekvence) * (j + krok) - Fin));
+
+                    listBoxHodnoty.Items.Add("Konst. " + i + " / prev / " + yPrev);
+                    listBoxHodnoty.Items.Add("Konst. " + i + " / next / " + yNext);
+
+                    grafHarmonicka.DrawLine(Pens.Black, j, yPrev, j + krok, yNext);
+                }
+            }
         }
     }
 }
