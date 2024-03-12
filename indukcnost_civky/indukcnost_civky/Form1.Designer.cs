dbf1a418f203166dc67724f1e81651a135fe8506
setrias <tomas.buchtik@borsice.net>
2024-03-12 14:29:53 +0100

    Indukčnost cívky WIP

--- indukcnost_civky/indukcnost_civky/Form1.Designer.cs
+++ indukcnost_civky/indukcnost_civky/Form1.Designer.cs
@@ -44,27 +44,28 @@
             this.textBoxMaxIndukcnost = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBoxPocetNadPetV = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxNapeti
             // 
-            this.textBoxNapeti.Location = new System.Drawing.Point(173, 50);
+            this.textBoxNapeti.Location = new System.Drawing.Point(173, 58);
             this.textBoxNapeti.Name = "textBoxNapeti";
-            this.textBoxNapeti.Size = new System.Drawing.Size(113, 32);
+            this.textBoxNapeti.Size = new System.Drawing.Size(113, 24);
             this.textBoxNapeti.TabIndex = 1;
             // 
             // textBoxProud
             // 
-            this.textBoxProud.Location = new System.Drawing.Point(173, 88);
+            this.textBoxProud.Location = new System.Drawing.Point(173, 96);
             this.textBoxProud.Name = "textBoxProud";
-            this.textBoxProud.Size = new System.Drawing.Size(113, 32);
+            this.textBoxProud.Size = new System.Drawing.Size(113, 24);
             this.textBoxProud.TabIndex = 2;
             // 
             // buttonZadat
             // 
-            this.buttonZadat.Location = new System.Drawing.Point(292, 15);
+            this.buttonZadat.Location = new System.Drawing.Point(292, 20);
             this.buttonZadat.Name = "buttonZadat";
-            this.buttonZadat.Size = new System.Drawing.Size(107, 105);
+            this.buttonZadat.Size = new System.Drawing.Size(107, 100);
             this.buttonZadat.TabIndex = 3;
             this.buttonZadat.Text = "Zadat hodnoty";
             this.buttonZadat.UseVisualStyleBackColor = true;
@@ -73,109 +74,121 @@
             // listBoxHodnoty
             // 
             this.listBoxHodnoty.FormattingEnabled = true;
-            this.listBoxHodnoty.ItemHeight = 24;
-            this.listBoxHodnoty.Location = new System.Drawing.Point(12, 272);
+            this.listBoxHodnoty.ItemHeight = 16;
+            this.listBoxHodnoty.Location = new System.Drawing.Point(12, 258);
             this.listBoxHodnoty.Name = "listBoxHodnoty";
-            this.listBoxHodnoty.Size = new System.Drawing.Size(387, 316);
+            this.listBoxHodnoty.Size = new System.Drawing.Size(387, 164);
             this.listBoxHodnoty.TabIndex = 4;
             // 
             // panelGraf
             // 
             this.panelGraf.Location = new System.Drawing.Point(416, 12);
             this.panelGraf.Name = "panelGraf";
-            this.panelGraf.Size = new System.Drawing.Size(719, 576);
+            this.panelGraf.Size = new System.Drawing.Size(719, 470);
             this.panelGraf.TabIndex = 5;
+            this.panelGraf.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGraf_Paint);
             // 
             // textBoxOdporCivky
             // 
-            this.textBoxOdporCivky.Location = new System.Drawing.Point(173, 12);
+            this.textBoxOdporCivky.Location = new System.Drawing.Point(173, 20);
             this.textBoxOdporCivky.Name = "textBoxOdporCivky";
-            this.textBoxOdporCivky.Size = new System.Drawing.Size(113, 32);
+            this.textBoxOdporCivky.Size = new System.Drawing.Size(113, 24);
             this.textBoxOdporCivky.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Location = new System.Drawing.Point(12, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 24);
+            this.label1.Size = new System.Drawing.Size(64, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Napětí [V]:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Location = new System.Drawing.Point(12, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.Size = new System.Drawing.Size(72, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Proud [mA]:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Location = new System.Drawing.Point(12, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(155, 24);
+            this.label3.Size = new System.Drawing.Size(111, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Odpor cívky [Ohm]:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 146);
+            this.label4.Location = new System.Drawing.Point(12, 139);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(170, 24);
+            this.label4.Size = new System.Drawing.Size(121, 17);
             this.label4.TabIndex = 10;
             this.label4.Text = "Indukčnost cívky [H]:";
             // 
             // textBoxIndukcnost
             // 
-            this.textBoxIndukcnost.Location = new System.Drawing.Point(209, 143);
+            this.textBoxIndukcnost.Location = new System.Drawing.Point(209, 136);
             this.textBoxIndukcnost.Name = "textBoxIndukcnost";
             this.textBoxIndukcnost.ReadOnly = true;
-            this.textBoxIndukcnost.Size = new System.Drawing.Size(190, 32);
+            this.textBoxIndukcnost.Size = new System.Drawing.Size(190, 24);
             this.textBoxIndukcnost.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 184);
+            this.label5.Location = new System.Drawing.Point(12, 177);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(191, 24);
+            this.label5.Size = new System.Drawing.Size(135, 17);
             this.label5.TabIndex = 12;
             this.label5.Text = "Největší indukčnost [H]:";
             // 
             // textBoxMaxIndukcnost
             // 
-            this.textBoxMaxIndukcnost.Location = new System.Drawing.Point(209, 181);
+            this.textBoxMaxIndukcnost.Location = new System.Drawing.Point(209, 174);
             this.textBoxMaxIndukcnost.Name = "textBoxMaxIndukcnost";
             this.textBoxMaxIndukcnost.ReadOnly = true;
-            this.textBoxMaxIndukcnost.Size = new System.Drawing.Size(190, 32);
+            this.textBoxMaxIndukcnost.Size = new System.Drawing.Size(190, 24);
             this.textBoxMaxIndukcnost.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 222);
+            this.label6.Location = new System.Drawing.Point(12, 215);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(152, 24);
+            this.label6.Size = new System.Drawing.Size(103, 17);
             this.label6.TabIndex = 14;
             this.label6.Text = "Pocet mereni > 5V:";
             // 
             // textBoxPocetNadPetV
             // 
-            this.textBoxPocetNadPetV.Location = new System.Drawing.Point(209, 219);
+            this.textBoxPocetNadPetV.Location = new System.Drawing.Point(209, 212);
             this.textBoxPocetNadPetV.Name = "textBoxPocetNadPetV";
             this.textBoxPocetNadPetV.ReadOnly = true;
-            this.textBoxPocetNadPetV.Size = new System.Drawing.Size(190, 32);
+            this.textBoxPocetNadPetV.Size = new System.Drawing.Size(190, 24);
             this.textBoxPocetNadPetV.TabIndex = 13;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(12, 437);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(387, 45);
+            this.buttonReset.TabIndex = 15;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1147, 600);
+            this.ClientSize = new System.Drawing.Size(1147, 497);
+            this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxPocetNadPetV);
             this.Controls.Add(this.label5);
@@ -198,6 +211,8 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button buttonReset;
 
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxMaxIndukcnost;
--- indukcnost_civky/indukcnost_civky/Form1.cs
+++ indukcnost_civky/indukcnost_civky/Form1.cs
@@ -28,6 +28,10 @@ namespace indukcnost_civky
         private double frekvence = 50, odporCivky;
         private int pocetHodnot;
 
+        private Graphics graf;
+        private int sirka, vyska;
+        private Pen peroGraf = new Pen(Color.Blue, 2);
+
         private void buttonZadat_Click(object sender, EventArgs e)
         {
             try
@@ -47,23 +51,11 @@ namespace indukcnost_civky
 
                         // vypocet indukcnosti
                         double omega = 2 * Math.PI * frekvence;
-                        double indukcnost = 
-                            Math.Sqrt(
-                                Math.Pow(
-                                    napeti, 2
-                                    ) / 
-                                Math.Pow(
-                                    proud / 1000, 2
-                                    ) + 
-                                Math.Pow(
-                                    odporCivky, 2
-                                    )
-                                ) 
-                            / omega;
+                        double indukcnost = Math.Sqrt(Math.Pow(napeti, 2) / Math.Pow(proud / 1000, 2) + Math.Pow(odporCivky, 2)) / omega;
 
                         mereni[pocetHodnot].indukcnost = indukcnost;
 
-                        listBoxHodnoty.Items.Add($"U = {napeti}, I = {proud}, L = {indukcnost}");
+                        listBoxHodnoty.Items.Add($"U = {napeti}V, I = {proud}mA, L = {indukcnost}H");
 
                         textBoxOdporCivky.Enabled = false;
                         pocetHodnot++;
@@ -72,6 +64,8 @@ namespace indukcnost_civky
                         {
                             buttonZadat.Enabled = false;
                             Vysledky();
+
+                            VykreslitGraf();
                         }
                     }
                     else
@@ -117,6 +111,50 @@ namespace indukcnost_civky
             textBoxIndukcnost.Text = Math.Round(soucetIndukcnost / 10, 1).ToString();
             textBoxMaxIndukcnost.Text = maxL.ToString();
             textBoxPocetNadPetV.Text = pocetMereniNadPetV.ToString();
+        }
+
+        private void VykreslitGraf()
+        {
+            float krokX = sirka / 10, krokY = vyska / 10;
+            float posunX = 0, posunY = 0;
+
+            graf.DrawLine(peroGraf, 0, 0, (float)mereni[0].napeti, (float)mereni[0].proud);
+
+            for (int i = 1; i < 10; i++)
+            {
+                float napetiPrev = (float)mereni[i - 1].napeti, proudPrev = (float)mereni[i - 1].proud;
+                float napeti = (float)mereni[i].napeti, proud = (float)mereni[i].proud;
+
+                graf.DrawLine(peroGraf, napetiPrev + posunX, vyska - proudPrev + posunY, napeti + posunX, vyska - proud + posunY);
+
+                posunX += krokX;
+                posunY += krokY;
+            }
+        }
+
+        private void panelGraf_Paint(object sender, PaintEventArgs e)
+        {
+            graf = panelGraf.CreateGraphics();
+
+            sirka = panelGraf.Width;
+            vyska = panelGraf.Height;
+
+            graf.DrawLine(Pens.Black, 0, vyska / 2, sirka, vyska / 2);
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            mereni = new HodnotyMereni[10];
+            pocetHodnot = 0;
+            buttonZadat.Enabled = true;
+            textBoxOdporCivky.Enabled = true;
+
+            textBoxOdporCivky.Clear();
+            textBoxMaxIndukcnost.Clear();
+            textBoxIndukcnost.Clear();
+            textBoxPocetNadPetV.Clear();
+            textBoxNapeti.Clear();
+            textBoxProud.Clear();
         }
     }
 }
