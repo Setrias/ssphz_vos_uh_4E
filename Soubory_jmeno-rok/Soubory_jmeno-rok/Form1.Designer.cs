184606edce8aceab9522298ddbe8b2dd2952369d
Setrias <tomas.buchtik@borsice.net>
2024-04-15 23:32:03 +0200

    File dialogy, práce se soubory úprava pro úkol

--- Soubory_jmeno-rok/Soubory_jmeno-rok/Form1.Designer.cs
+++ Soubory_jmeno-rok/Soubory_jmeno-rok/Form1.Designer.cs
@@ -35,14 +35,20 @@
             this.textBoxJmeno = new System.Windows.Forms.TextBox();
             this.textBoxRok = new System.Windows.Forms.TextBox();
             this.buttonPrecist = new System.Windows.Forms.Button();
+            this.buttonVyberRok = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxVyber = new System.Windows.Forms.ListBox();
+            this.numericUpDownVyberRok = new System.Windows.Forms.NumericUpDown();
+            this.buttonVybratSoubor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVyberRok)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonUlozit
             // 
-            this.buttonUlozit.Location = new System.Drawing.Point(200, 22);
+            this.buttonUlozit.Location = new System.Drawing.Point(292, 22);
             this.buttonUlozit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonUlozit.Name = "buttonUlozit";
-            this.buttonUlozit.Size = new System.Drawing.Size(182, 27);
+            this.buttonUlozit.Size = new System.Drawing.Size(107, 27);
             this.buttonUlozit.TabIndex = 0;
             this.buttonUlozit.Text = "Uložit do souboru";
             this.buttonUlozit.UseVisualStyleBackColor = true;
@@ -85,17 +91,73 @@
             this.buttonPrecist.Location = new System.Drawing.Point(200, 57);
             this.buttonPrecist.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonPrecist.Name = "buttonPrecist";
-            this.buttonPrecist.Size = new System.Drawing.Size(182, 27);
+            this.buttonPrecist.Size = new System.Drawing.Size(199, 27);
             this.buttonPrecist.TabIndex = 9;
             this.buttonPrecist.Text = "Vybrat a přečíst soubor";
             this.buttonPrecist.UseVisualStyleBackColor = true;
             this.buttonPrecist.Click += new System.EventHandler(this.buttonPrecist_Click);
             // 
+            // buttonVyberRok
+            // 
+            this.buttonVyberRok.Location = new System.Drawing.Point(200, 165);
+            this.buttonVyberRok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonVyberRok.Name = "buttonVyberRok";
+            this.buttonVyberRok.Size = new System.Drawing.Size(199, 38);
+            this.buttonVyberRok.TabIndex = 12;
+            this.buttonVyberRok.Text = "Vypsat lidi";
+            this.buttonVyberRok.UseVisualStyleBackColor = true;
+            this.buttonVyberRok.Click += new System.EventHandler(this.buttonVyberRok_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Zadaný rok [1900 - 2100]:";
+            // 
+            // listBoxVyber
+            // 
+            this.listBoxVyber.FormattingEnabled = true;
+            this.listBoxVyber.ItemHeight = 16;
+            this.listBoxVyber.Location = new System.Drawing.Point(12, 112);
+            this.listBoxVyber.Name = "listBoxVyber";
+            this.listBoxVyber.Size = new System.Drawing.Size(182, 260);
+            this.listBoxVyber.TabIndex = 13;
+            // 
+            // numericUpDownVyberRok
+            // 
+            this.numericUpDownVyberRok.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDownVyberRok.Location = new System.Drawing.Point(200, 131);
+            this.numericUpDownVyberRok.Maximum = new decimal(new int[] { 2100, 0, 0, 0 });
+            this.numericUpDownVyberRok.Minimum = new decimal(new int[] { 1900, 0, 0, 0 });
+            this.numericUpDownVyberRok.Name = "numericUpDownVyberRok";
+            this.numericUpDownVyberRok.Size = new System.Drawing.Size(199, 27);
+            this.numericUpDownVyberRok.TabIndex = 14;
+            this.numericUpDownVyberRok.Value = new decimal(new int[] { 1900, 0, 0, 0 });
+            // 
+            // buttonVybratSoubor
+            // 
+            this.buttonVybratSoubor.Location = new System.Drawing.Point(200, 22);
+            this.buttonVybratSoubor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonVybratSoubor.Name = "buttonVybratSoubor";
+            this.buttonVybratSoubor.Size = new System.Drawing.Size(86, 27);
+            this.buttonVybratSoubor.TabIndex = 15;
+            this.buttonVybratSoubor.Text = "Vybrat soubor";
+            this.buttonVybratSoubor.UseVisualStyleBackColor = true;
+            this.buttonVybratSoubor.Click += new System.EventHandler(this.buttonVybratSoubor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 96);
+            this.ClientSize = new System.Drawing.Size(411, 388);
+            this.Controls.Add(this.buttonVybratSoubor);
+            this.Controls.Add(this.numericUpDownVyberRok);
+            this.Controls.Add(this.listBoxVyber);
+            this.Controls.Add(this.buttonVyberRok);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.buttonPrecist);
             this.Controls.Add(this.textBoxRok);
             this.Controls.Add(this.textBoxJmeno);
@@ -106,9 +168,19 @@
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Soubory";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVyberRok)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button buttonVybratSoubor;
+
+        private System.Windows.Forms.NumericUpDown numericUpDownVyberRok;
+
+        private System.Windows.Forms.ListBox listBoxVyber;
+
+        private System.Windows.Forms.Button buttonVyberRok;
+        private System.Windows.Forms.Label label3;
 
         private System.Windows.Forms.Button buttonUlozit;
 
--- Soubory_jmeno-rok/Soubory_jmeno-rok/Form1.cs
+++ Soubory_jmeno-rok/Soubory_jmeno-rok/Form1.cs
@@ -9,58 +9,109 @@ namespace Soubory_jmeno_rok
         public Form1()
         {
             InitializeComponent();
+            buttonVyberRok.Enabled = false;
         }
 
+        struct Udaje
+        {
+            public string jmeno;
+            public int rok;
+        }
+
+        private Udaje[] _udaje;
         private string jmeno, rok;
         private StreamReader sReader;
         private StreamWriter sWriter;
+        private string soubor;
 
-        private void buttonUlozit_Click(object sender, EventArgs e)
+        private void buttonVybratSoubor_Click(object sender, EventArgs e)
         {
-            jmeno = textBoxJmeno.Text;
-            rok = textBoxRok.Text;
-
             // vytvoření instance dialogu pro uložení souboru
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.InitialDirectory = Directory.GetCurrentDirectory(); // výchozí složka po otevření dialogu
             saveFileDialog.FileName = "soubor.txt"; // výchozí název souboru
-            saveFileDialog.Filter = "Textové soubory (*.txt)|.txt"; // filtr souborů pro omezení výběru pouze na .txt soubory
+            saveFileDialog.Filter = "Textové soubory (*.txt)|*.txt"; // filtr souborů pro omezení výběru pouze na .txt soubory
 
-            string file;
             // výběr / pojmenování souboru
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 // po výběru / pojmenování souboru se převezme název souboru
-                file = saveFileDialog.FileName;
-                sWriter = new StreamWriter(file); // vytvoří se StreamWriter s vybraným souborem
-                sWriter.Write($"{jmeno} {rok}");
-                sWriter.Flush();
-                sWriter.Close();
+                soubor = saveFileDialog.FileName;
             }
+        }
+
+        private void buttonUlozit_Click(object sender, EventArgs e)
+        {
+            jmeno = textBoxJmeno.Text.Trim();
+            rok = textBoxRok.Text.Trim();
+
+            if (jmeno.Contains(" "))
+            {
+                MessageBox.Show("Zadej POUZE jméno.");
+                return;
+            }
+
+            sWriter = new StreamWriter(soubor, true); // vytvoří se StreamWriter s vybraným souborem
+            sWriter.Write($"{jmeno} {rok}\n");
+            sWriter.Flush();
+            sWriter.Close();
         }
 
         private void buttonPrecist_Click(object sender, EventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            fileDialog.Filter = "Textové soubory (*.txt)|*.txt"; // filtr souborů pro omezení výběru pouze na .txt soubory
+
             string file;
+            int radek = 0;
 
             if (fileDialog.ShowDialog() == DialogResult.OK)
             {
-                file = fileDialog.FileName;
-                sReader = new StreamReader(file);
-                string souborRadek;
+                file = fileDialog.FileName; // název souboru
 
+                // přečte všechny řádky souboru a spočítá je (JE TO NÁROČNÉ NA MEMORY)
+                int pocetRadku = File.ReadAllLines(file).Length;
+                _udaje = new Udaje[pocetRadku]; // vytvoření pole záznamů o velikosti počtu řádků
+
+                sReader = new StreamReader(file);
+
+                string souborRadek;
                 while ((souborRadek = sReader.ReadLine()) != null)
                 {
+                    // rozdělení na jméno a rok
                     jmeno = souborRadek.Split(' ')[0];
                     rok = souborRadek.Split(' ')[1];
 
-                    textBoxJmeno.Text = jmeno;
-                    textBoxRok.Text = rok;
+                    try
+                    {
+                        _udaje[radek].jmeno = jmeno;
+                        _udaje[radek].rok = Convert.ToInt32(rok);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Chyba při čtení dat ze souboru. Data musí být ve správném formátu ([string]{jmeno} [int]{rok}).");
+                    }
+
+                    radek++;
                 }
 
                 sReader.Close();
+                buttonVyberRok.Enabled = true;
+            }
+        }
+
+        private void buttonVyberRok_Click(object sender, EventArgs e)
+        {
+            listBoxVyber.Items.Clear();
+            int zadanyRok = (int)numericUpDownVyberRok.Value;
+
+            foreach (var udaj in _udaje)
+            {
+                if (udaj.rok == zadanyRok)
+                {
+                    listBoxVyber.Items.Add(udaj.jmeno);
+                }
             }
         }
     }
