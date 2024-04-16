15de5867aa0c24947e4b5da3f4233f1c4980f695
setrias <tomas.buchtik@borsice.net>
2024-04-16 14:19:05 +0200

    Soubory děti, dodělat graf

---
+++ Soubory_deti/Soubory_deti/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Soubory_deti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxUdaje = new System.Windows.Forms.ListBox();
+            this.panelGraf = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPocetTriletych = new System.Windows.Forms.TextBox();
+            this.textBoxPrumVyskaDvouletych = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPrumVahaDvouletych = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxUdaje
+            // 
+            this.listBoxUdaje.FormattingEnabled = true;
+            this.listBoxUdaje.ItemHeight = 16;
+            this.listBoxUdaje.Location = new System.Drawing.Point(12, 12);
+            this.listBoxUdaje.Name = "listBoxUdaje";
+            this.listBoxUdaje.Size = new System.Drawing.Size(249, 388);
+            this.listBoxUdaje.TabIndex = 0;
+            // 
+            // panelGraf
+            // 
+            this.panelGraf.Location = new System.Drawing.Point(267, 12);
+            this.panelGraf.Name = "panelGraf";
+            this.panelGraf.Size = new System.Drawing.Size(600, 400);
+            this.panelGraf.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Počet tříletých dětí:";
+            // 
+            // textBoxPocetTriletych
+            // 
+            this.textBoxPocetTriletych.Location = new System.Drawing.Point(171, 410);
+            this.textBoxPocetTriletych.Name = "textBoxPocetTriletych";
+            this.textBoxPocetTriletych.Size = new System.Drawing.Size(90, 23);
+            this.textBoxPocetTriletych.TabIndex = 3;
+            // 
+            // textBoxPrumVyskaDvouletych
+            // 
+            this.textBoxPrumVyskaDvouletych.Location = new System.Drawing.Point(171, 439);
+            this.textBoxPrumVyskaDvouletych.Name = "textBoxPrumVyskaDvouletych";
+            this.textBoxPrumVyskaDvouletych.Size = new System.Drawing.Size(90, 23);
+            this.textBoxPrumVyskaDvouletych.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Průměrná výška dvouletých:";
+            // 
+            // textBoxPrumVahaDvouletych
+            // 
+            this.textBoxPrumVahaDvouletych.Location = new System.Drawing.Point(171, 468);
+            this.textBoxPrumVahaDvouletych.Name = "textBoxPrumVahaDvouletych";
+            this.textBoxPrumVahaDvouletych.Size = new System.Drawing.Size(90, 23);
+            this.textBoxPrumVahaDvouletych.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 471);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Průměrná váha dvouletých:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 506);
+            this.Controls.Add(this.textBoxPrumVahaDvouletych);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPrumVyskaDvouletych);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPocetTriletych);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelGraf);
+            this.Controls.Add(this.listBoxUdaje);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Soubory děti";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxPrumVahaDvouletych;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPocetTriletych;
+        private System.Windows.Forms.TextBox textBoxPrumVyskaDvouletych;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.ListBox listBoxUdaje;
+        private System.Windows.Forms.Panel panelGraf;
+
+        #endregion
+    }
+}
---
+++ Soubory_deti/Soubory_deti/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Soubory_deti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct Udaje
+        {
+            public string jmeno;
+            public int vek;
+            public double vyska;
+            public double vaha;
+        }
+
+        private Udaje[] _udaje;
+        private StreamReader sReader;
+        private string nazevSouboru = "deti.txt";
+        private int pocetUdaju;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Data();
+            PocetTriletych();
+            Dvoulete();
+        }
+
+        private void Data()
+        {
+            if (File.Exists(nazevSouboru))
+            {
+                sReader = new StreamReader(nazevSouboru);
+                string[] radek;
+
+                // počet řádků souboru
+                int pocetRadku = File.ReadLines(nazevSouboru).Count();
+                _udaje = new Udaje[pocetRadku];
+
+                string souborRadek;
+                while ((souborRadek = sReader.ReadLine()) != null)
+                {
+                    // rozdělení na jednotlivé části
+                    radek = souborRadek.Trim().Split(' ');
+
+                    // uložení a vypsání dat
+                    try
+                    {
+                        _udaje[pocetUdaju].jmeno = radek[0];
+                        _udaje[pocetUdaju].vek = Convert.ToInt32(radek[2]);
+                        _udaje[pocetUdaju].vyska = Convert.ToDouble(radek[4]);
+                        _udaje[pocetUdaju].vaha = Convert.ToDouble(radek[6]);
+
+                        listBoxUdaje.Items.Add($"Jméno: {radek[0]}, věk: {radek[2]}, výška: {radek[4]}, váha: {radek[6]}");
+
+                        pocetUdaju++;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Data v souboru mají špatný formát.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Soubor neexistuje");
+            }
+        }
+
+        private void PocetTriletych()
+        {
+            int pocetTriletych = 0;
+
+            foreach (var udaj in _udaje)
+            {
+                if (udaj.vek == 3)
+                {
+                    pocetTriletych++;
+                }
+            }
+
+            textBoxPocetTriletych.Text = pocetTriletych.ToString();
+        }
+
+        private void Dvoulete()
+        {
+            double soucetVyska = 0, soucetVaha = 0;
+            int pocetDvouletych = 0;
+
+            foreach (var udaj in _udaje)
+            {
+                if (udaj.vek == 2)
+                {
+                    pocetDvouletych++;
+                    soucetVyska += udaj.vyska;
+                    soucetVaha += udaj.vaha;
+                }
+            }
+
+            textBoxPrumVyskaDvouletych.Text = (soucetVyska / pocetDvouletych).ToString();
+            textBoxPrumVahaDvouletych.Text = (soucetVaha / pocetDvouletych).ToString();
+        }
+
+        private void Graf()
+        {
+            // podle zadání [výška v cm, hmotnost v kg] ([x, y])
+        }
+    }
+}
